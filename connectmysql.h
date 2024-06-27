e854e663a7f6b8b5cebe824e8b3c7f275459c8d4
DevJack1232 <83761854+DevJack1232@users.noreply.github.com>
2024-06-27 10:20:01 +0300

    Add files via upload

---
+++ connectmysql.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "C:\MySQL\mysql-connector-c++-8.3.0-winx64\include\jdbc\cppconn\connection.h"
+#include "C:\MySQL\mysql-connector-c++-8.3.0-winx64\include\jdbc\mysql_connection.h"
+#include "C:\MySQL\mysql-connector-c++-8.3.0-winx64\include\jdbc\cppconn\driver.h"
+#include "C:\MySQL\mysql-connector-c++-8.3.0-winx64\include\jdbc\cppconn\exception.h"
+#include "C:\MySQL\mysql-connector-c++-8.3.0-winx64\include\jdbc\cppconn\prepared_statement.h"
+#include "C:\MySQL\mysql-connector-c++-8.3.0-winx64\include\jdbc\cppconn\resultset.h"
+#include "C:\MySQL\mysql-connector-c++-8.3.0-winx64\include\jdbc\cppconn\statement.h"
